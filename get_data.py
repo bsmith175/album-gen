2d52b229a9dd59c134502eea196947d41af22c2e
bsmith175 <benlismith@gmail.com>
2020-11-27 19:34:54 -0600

    make data

--- get_data.py
+++ get_data.py
@@ -1,12 +1,13 @@
 import requests
 import util
 import csv
+import os
+from PIL import Image
 
 API_KEY = 'ZWY5YmEzNTItNjczMS00ODEzLTg2ZTMtNjYxMDkzMmJkY2Jj'
 API = 'https://api.napster.com/'
 IMG_PER_ALBUM = 1000
 IMG_DIR = './data/'
-
 genres = ['rock', 'reggae', 'jazz', 'pop', 'country', 'rap-hip-hop', 'classical']
 
 def get_genre_ids(shortcuts):
@@ -17,13 +18,24 @@ def get_genre_ids(shortcuts):
 
 def get_imgs_from_album(album_ids):
     response = requests.get(API + 'v2.2/albums/' + ",".join(album_ids) + '/images?apikey=' + API_KEY).json()['images']
-    urls = list(map(lambda item: item['url'], response))
+    content_ids = set()
+    urls = []
+    for album in response:
+        if album['contentId'] not in content_ids:
+            urls.append(album['url'])
+            content_ids.add(album['contentId'])
+    print(len(urls))
     return urls
 
 covers_for_genre = {}
 for id, name in get_genre_ids(genres):
-    print('Getting images for genre: ' + name + ' (' + id + ')' '...')
+    print('\n\nGetting images for genre: ' + name + ' (' + id + ')' '...')
     covers_for_genre[name] = []
+
+    if not os.path.exists(IMG_DIR + name):
+        print("making directory: " + IMG_DIR + name)
+        os.makedirs(IMG_DIR + name)
+
     for i in range(0, IMG_PER_ALBUM, 200):
         print('\nbatch ' + str(int(i / 200)) + "...")
         albums = requests.get(API + 'v2.2/genres/' + id + '/albums/top?limit=200&offset=' + str(i) + '&apikey=' + API_KEY).json()['albums']
@@ -31,11 +43,18 @@ for id, name in get_genre_ids(genres):
         ids = list(map(lambda alb: alb['id'], albums))
         urls = get_imgs_from_album(ids)
         covers_for_genre[name].append(urls)
+        for ix, url  in enumerate(urls):
+            response = requests.get(url, stream=True)
+            if response.status_code == 200:
+                img = Image.open(response.raw)
+                img.save(IMG_DIR + name + '/' + str(i + ix) + '.jpg')
+            else:
+                print("Error: " + str(response.status_code))
 
-    with open(IMG_DIR + name + '.csv', 'w', newline='') as cur_file:
-        wr = csv.writer(cur_file)
-        print("writing to " + name + '.csv')
-        wr.writerow(covers_for_genre[name])
+    # with open(IMG_DIR + name + '.csv', 'w', newline='') as cur_file:
+    #     wr = csv.writer(cur_file)
+    #     print("\nwriting to " + name + '.csv')
+    #     wr.writerow(covers_for_genre[name])
 
 
 
