082acc2319dac72ae2cb918c5ca3607978a421d2
James Tsatsaros <jtsatsaros@MacBook-Pro-8.fios-router.home>
2020-12-03 17:40:00 -0500

    added gan

---
+++ gan.py
@@ -0,0 +1,79 @@
+from discriminator import Discriminator
+from generator import Generator
+from preprocess import get_data
+import torch
+import numpy as np
+from PIL import Image
+
+
+def main():
+
+    discriminator = Discriminator()
+    generator = Generator()
+    num_epochs = 1
+    batch_size = 128
+    cat_dim = 7
+    con_dim = 2
+    rand_dim = 100
+
+    ##call .eval() before testing
+    ##and call .train() if you are going to train again
+
+    for epoch in range(num_epochs):
+        for real_images, cat_labels in get_data('data/inputs.npy', 'data/labels.npy', batch_size):
+            real_images = torch.from_numpy(real_images)
+            cat_labels = torch.from_numpy(cat_labels).long()
+            real_logits, real_cat_logits, _ = discriminator(real_images)
+
+            discriminator.zero_grad()
+            d_real_loss = discriminator.loss(real_logits, torch.ones_like(cat_labels))
+            d_real_cat_loss = discriminator.loss(real_cat_logits, cat_labels)
+            real_d_score = d_real_loss + d_real_cat_loss * 10
+
+            z_cat = torch.Tensor(np.random.uniform(0, 1, size=[batch_size, cat_dim]).astype(np.float32))
+            z_con = torch.Tensor(np.random.uniform(-1, 1, size=[batch_size, con_dim]).astype(np.float32))
+            z_rand = torch.Tensor(np.random.uniform(-1, 1, size=[batch_size, rand_dim]).astype(np.float32))
+            fake_images = generator(z_cat, z_con, z_rand)
+            fake_logits, fake_cat_logits, latent_logits = discriminator(fake_images.detach())
+            fake_labels = torch.zeros((fake_logits.shape[0],)).long()
+            d_fake_loss = discriminator.loss(fake_logits, fake_labels)
+            cats = torch.argmax(z_cat, 1)
+            d_fake_cat_loss = discriminator.loss(fake_cat_logits, cats)
+            latent_loss = discriminator.latent_loss(latent_logits, z_con)
+            fake_d_score = d_fake_loss + d_fake_cat_loss * 10 + latent_loss
+            d_score = real_d_score + fake_d_score
+            d_score.backward()
+            discriminator.optimizer.step()
+
+            generator.zero_grad()
+            fake_logits, fake_cat_logits, _ = discriminator(fake_images)
+            cats = torch.argmax(z_cat, 1)
+            d_fake_cat_loss = discriminator.loss(fake_cat_logits, cats)
+            g_loss = generator.loss(fake_logits)
+            g_score = g_loss + d_fake_cat_loss * 10
+            g_score.backward()
+            generator.optimizer.step()
+
+    generator.eval()
+    test_size = 1
+    z_cat = torch.Tensor(np.random.uniform(0, 1, size=[test_size, cat_dim]).astype(np.float32))
+    z_con = torch.Tensor(np.random.uniform(-1, 1, size=[test_size, con_dim]).astype(np.float32))
+    z_rand = torch.Tensor(np.random.uniform(-1, 1, size=[test_size, rand_dim]).astype(np.float32))
+    img = generator(z_cat, z_con, z_rand).detach().numpy()
+    img = np.rollaxis(img,1, 4)
+    img = (img+1) * 127.5
+    img = img.astype(np.uint8)
+    out_dir = '/Users/jtsatsaros/Documents/album-gen'
+    # Convert to uint8
+    # Save images to disk
+    for i in range(0, test_size):
+        img_i = img[i]
+        s = out_dir + '/' + str(i) + '.png'
+        img_i = Image.fromarray(img_i)
+        img_i.save(s)
+
+
+
+
+if __name__ == '__main__':
+   main()
--- generator.py
+++ generator.py
@@ -9,60 +9,39 @@ class Generator(torch.nn.Module):
         self.batch_size = 128
         self.stride = 2
         self.kernel_size = 5
-
-        self.cat_dim = 2
+        self.cat_dim = 7
         self.con_dim = 2
-        self.rand_dim = 100 #idk what these are needed for
+        self.rand_dim = 100
         self.noise_dim = self.cat_dim + self.con_dim + self.rand_dim
-        self.padding = 0
+        self.pad = 2
+        self.out_pad = 1
 
-        self.dense = torch.nn.Linear(self.noise_dim, 8192, True) #linear: in_feats, out_feats, bias
-        self.norm1 = torch.nn.BatchNorm2d(512) #paper's batch norm uses tf maximum?
+        self.dense = torch.nn.Linear(self.noise_dim, 8192)
+        self.convt1 = torch.nn.ConvTranspose2d(512, 256, self.kernel_size, stride=self.stride, padding=self.pad, output_padding=self.out_pad)
+        self.norm1 = torch.nn.BatchNorm2d(512)
+        self.convt2 = torch.nn.ConvTranspose2d(256, 128, self.kernel_size, stride=self.stride, padding=self.pad, output_padding=self.out_pad)
         self.norm2 = torch.nn.BatchNorm2d(256)
+        self.convt3 = torch.nn.ConvTranspose2d(128, 64, self.kernel_size, stride=self.stride, padding=self.pad, output_padding=self.out_pad)
         self.norm3 = torch.nn.BatchNorm2d(128)
+        self.convt4 = torch.nn.ConvTranspose2d(64, 3, self.kernel_size, stride=self.stride, padding=self.pad, output_padding=self.out_pad)
         self.norm4 = torch.nn.BatchNorm2d(64)
-        self.convt1 = torch.nn.ConvTranspose2d(512, 256, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding)
-        self.convt2 = torch.nn.ConvTranspose2d(256, 128, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding)
-        self.convt3 = torch.nn.ConvTranspose2d(128, 64, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding)
-        self.convt4 = torch.nn.ConvTranspose2d(64, 3, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding)
+        self.learning_rate = 1e-5
+        self.beta1 = 0.5
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate, betas=(self.beta1, 0.999))
 
-        self.optimizer = torch.optim.Adam(self.parameters(), 1e-3)
-
-    def call(self, cat, con, rand):
+    def forward(self, cat, con, rand):
         noise = torch.cat([cat, con, rand], 1)
         g0 = self.dense(noise)
-        #batchNorm2d checks 2nd index for num_features...doing this reshape instead of (self.batch_size, 4, 4, 512)
-        g1 = torch.reshape(g0, (self.batch_size, 512, 4, 4))
-        print("g1 shape: ", g1.shape)
-        g1_b = self.norm1(g1)
-        g2 = torch.nn.functional.leaky_relu(self.convt1(g1_b), negative_slope=0.1)
-        print("g2 shape: ", g2.shape)
-        g2_b = self.norm2(g2)
-        g3 = torch.nn.functional.leaky_relu(self.convt2(g2_b), negative_slope=0.1)
-        print("g3 shape: ", g3.shape)
-        g3_b = self.norm3(g3)
-        g4 = torch.nn.functional.leaky_relu(self.convt3(g3_b), negative_slope=0.1)
-        print("g4 shape: ", g4.shape)
-        g4_b = self.norm4(g4)
-        g5 = self.convt4(g4_b)
-        print("g5 shape: ", g5.shape)
+        g1 = self.norm1(torch.reshape(g0, (-1, 512, 4, 4)))
+        g2 = self.norm2(torch.nn.functional.relu(self.convt1(g1)))
+        g3 = self.norm3(torch.nn.functional.relu(self.convt2(g2)))
+        g4 = self.norm4(torch.nn.functional.relu(self.convt3(g3)))
+        g5 = self.convt4(g4)
         return torch.tanh(g5)
 
-    def loss(self, D_logits): #discriminator logits?
-        return torch.nn.functional.cross_entropy(logits=D_logits_, labels=torch.ones_like(D_logits), reduction='mean') #ones like?
-
-    def accuracy(self, logits, labels): # discriminator labels again?
-        predicted = torch.argmax(logits, 1)
-        matches = torch.eq(predicted, labels)
-        matches = matches.type(torch.FloatTensor)
-        return torch.mean(matches).item()
-
-def train(self, inputs, labels):
-    logits = self(inputs)
-    loss = self.loss(logits, labels)
-    loss.backward()
-    self.optimizer.step()
-    return self.accuracy(logits, labels)
+    def loss(self, fake_logits):
+        fake_labels = torch.ones((fake_logits.shape[0],)).long()
+        return torch.nn.functional.cross_entropy(fake_logits, fake_labels, reduction='mean')
 
 
 def main():
@@ -81,14 +60,13 @@ def main():
     gen = Generator()
     cat_dim = 2
     con_dim = 2
-    rand_dim = 100 #idk what these are needed for
+    rand_dim = 100
     noise_dim = cat_dim + con_dim + rand_dim
     batch_size = 128
     #testing shapes - Got device type cpu error??
-    cat = torch.Tensor(np.zeros((batch_size, cat_dim)))
-    con = torch.Tensor(np.zeros((batch_size, con_dim)))
-    rand = torch.Tensor(np.zeros((batch_size, rand_dim)))
-    gen.call(cat, con, rand)
+    cat = torch.Tensor(np.random.uniform(-1, 1, size=[batch_size, cat_dim]).astype(np.float32))
+    con = torch.Tensor(np.random.uniform(-1, 1, size=[batch_size, con_dim]).astype(np.float32))
+    rand = torch.Tensor(np.random.uniform(-1, 1, size=[batch_size, rand_dim]).astype(np.float32))
 
 
 
